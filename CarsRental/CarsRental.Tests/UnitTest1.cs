8f2fd2dcd67d1dde112a4b63f36a1f58fc7e9056
me <me@ME-KOMPUTER>
2016-06-22 21:35:30 +0200

    Pierwszy Commit

---
+++ CarsRental/CarsRental.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CarsRental.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
