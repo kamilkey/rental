dfae77e080865b4d087f02da3e424823671efd30
me <me@ME-KOMPUTER>
2016-06-25 19:31:39 +0200

    logowanie

--- CarsRental/CarsRental/Controllers/HomeController.cs
+++ CarsRental/CarsRental/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using CarsRental.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -12,7 +13,34 @@ namespace CarsRental.Controllers
         public ActionResult Index()
         {
             // testy gita
-
+            return View();
+        }
+        public ActionResult Login()
+        {
+            // testy gita
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Login(CarsRental.Models.RentalUsers u)
+        {
+            if (ModelState.IsValid)
+            {
+                using(RentalDBEntities db = new RentalDBEntities())
+                {
+                    var v = db.RentalUsers.Where(a => a.login.Equals(u.login) && a.password.Equals(u.password)).FirstOrDefault();
+                    if(v != null)
+                    {
+                        Session["UserID"] = v.userID.ToString();
+                        Session["Username"] = v.login.ToString();
+                        return RedirectToAction("Admin");
+                    }
+                }
+            }
+            return View(u);
+        }
+        public ActionResult Admin()
+        {
             return View();
         }
     }
